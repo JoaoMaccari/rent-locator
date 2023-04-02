6d3d1abcadcb0f15d5f72ca6e4e58da6954126ed
João Elias Maccari <joaomaccari@hotmail.com>
2023-04-02 17:03:21 -0300

    add
    add novos pontos

--- js/script.js
+++ js/script.js
@@ -18,9 +18,84 @@ var marker1 = L.marker([-28.652092, -49.208899],{
 }).addTo(map).bindPopup("<h1>Sala comercial</h1> <p>Sala 01: 54m2</p> " + 
                         "<p>Sala 02: 78m2</p>" + 
                         "<p>Sala 03: 47m2</p>" +
+                        "<p>Sala 03: 32m2</p>" +
                         "<img width='560' height='315' class='salaCentro' src ='./img/aluguel.png'/>",{
                             maxWidth : 560,
-                        } )
+});
+
+var marker2 = L.marker([-28.652835875502774, -49.209013485220595],{
+
+}).addTo(map).bindPopup("<h1>Sala comercial</h1> <p>Sala 01: 100m2</p> " + 
+                    "<p>Sala 02: 41m2</p>" + 
+                    "<p>Sala 03: 59m2</p>" +
+                    "<p>Sala 04: 59m2</p>" +
+                    "<img width='560' height='315' class='salaCentro' src ='./img/aluguel2.png'/>",{
+                        maxWidth : 560,
+});
+
+
+var marker3 = L.marker([-28.6523, -49.2106],{
+
+}).addTo(map).bindPopup("<h1>Apartamentos</h1> <p>Apto 01: 1 Andar - 185m2</p> " + 
+                    "<p>Apto 02: 2 Andar 200m2</p>" + 
+                    "<img width='560' height='315' class='salaCentro' src ='./img/aluguel3.png'/>",{
+                        maxWidth : 560,
+})
+
+
+var marker4 = L.marker([-28.6453, -49.1988],{
+
+}).addTo(map).bindPopup("<h1>Terreno</h1> <p> 01 terreno de  300m2</p> " + 
+                    "<img width='560' height='315' class='salaCentro' src ='./img/aluguel3.png'/>",{
+                        maxWidth : 560,
+})
+
+
+var marker5 = L.marker([-28.6457, -49.1984],{
+
+}).addTo(map).bindPopup("<h1>Terreno</h1> <p> 01 terreno de  300m2</p> " + 
+                        "<img width='560' height='315' class='salaCentro' src ='./img/aluguel3.png'/>",{
+                            maxWidth : 560,
+})
+
+var marker6 = L.marker([-28.7606, -49.1284],{
+
+}).addTo(map).bindPopup("<h1>Casa</h1> <p> Casa alvenaria 316m2</p> " + 
+                        "<img width='560' height='315' class='salaCentro' src ='./img/aluguel4.png'/>",{
+                            maxWidth : 560,
+});
+
+var marker7 = L.marker([-28.7601, -49.1256],{
+
+}).addTo(map).bindPopup("<h1>Apartamentos</h1> <p> Prédio com 18 aptos para alugar por temporada</p> " + 
+                       
+                        "<img width='560' height='315' class='salaCentro' src ='./img/aluguel5.png'/>",{
+                            maxWidth : 560,
+                           
+})
+
+var marker8 = L.marker([-28.4788, -48.7682],{
+
+}).addTo(map).bindPopup("<h1>Apartamento</h1> <p> Apartamento com 200m2</p> " + 
+                       
+                        "<img width='560' height='315' class='salaCentro' src ='./img/aluguel5.png'/>",{
+                            maxWidth : 560,
+                           
+})
+
+var marker9 = L.marker([-28.4875, -48.7670],{
+
+}).addTo(map).bindPopup("<h1>Apartamento</h1> <p> Apartamento com 200m2</p> " + 
+                       
+                        "<img width='560' height='315' class='salaCentro' src ='./img/aluguel5.png'/>",{
+                            maxWidth : 560,
+                           
+})
+
+
+
+
+
 
 
 // var marker = L.marker([-49.20, -28.65], {
